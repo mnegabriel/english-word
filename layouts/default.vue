50f56de2b277eccba293e6db5becb8c0ebed3dbc
Gabriel Dantas <gabrielmd123@gmail.com>
2024-05-11 13:48:12 -0300

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="index-layout">
+    <header class="container mx-auto px-4 bg-indigo-200 py-4">
+      <nav>
+        <ul>
+          <li class="font-bold">
+            <NuxtLink to="/">BusFortal</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <Nuxt />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container mx-auto px-4">
+    <h1 class="text-4xl">bus-fortal</h1>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
